6d564d240ae9038fc4c8dcdfbd6ca39c6d2d82d8
Alexander Oleinik <alxndr@solo.vrmnet>
2016-05-11 22:13:34 -0400

    draws a block. oops

---
+++ rapstar.pl
@@ -0,0 +1,38 @@
+#!/bin/perl
+#
+# Bling out your github profile by making a few commits here and there
+#
+
+use DateTime;
+
+my @pattern = ( # Whatever you want - just make sure there are 7 rows in the matrix
+[0,0,0,0,0,0,0,0],
+[1,0,0,1,0,0,1,0],
+[0,0,1,0,1,0,0,1],
+[0,1,0,0,0,1,0,1],
+[0,0,0,0,0,0,0,1],
+[1,0,0,0,0,0,1,0],
+[0,0,0,0,0,0,0,0]
+);
+
+my $date = DateTime->new( 
+                year   => 2015, 
+                month  => 7, 
+                day    => 5,    # You probably want this to be a sunday 
+                hour   => 10, 
+                minute => 30, 
+                second => 0, 
+                nanosecond => 500000000, 
+                time_zone => 'America/New_York' );
+
+my $rows = @pattern;
+my $cols = @{$pattern[0]}; # Assuming the number of columns is the same across all rows
+
+for (my $i=0; $i < $cols; $i++) {
+    for (my $j=0; $j < $rows; $j++) {
+        system("git", "commit", "--allow-empty", "-m", 
+            "This commit is literally nothing", "--date", $date  );
+        $date->add(days => 1); 
+    }
+}
+
